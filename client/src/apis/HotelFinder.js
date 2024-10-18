26429d74d22dcf432e754e5bbda9ae9ec0009d74
Ta Hong Phuc <taphuc1@gmail.com>
2024-10-19 02:23:50 +0700

    update vercel api

--- client/src/apis/HotelFinder.js
+++ client/src/apis/HotelFinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3010/api/v1/hotels",
+    baseURL: "http://hotel-booking-backend-azure.vercel.app/api/v1/hotels",
 })
